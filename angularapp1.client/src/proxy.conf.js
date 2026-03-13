c4eb1da57c09a392765a3b1dd87d2674256e8d79
a15963lleoallentw@yahoo.com.tw <a15963lleoallentw@yahoo.com.tw>
2026-03-13 09:56:29 +0800

    https改成http

--- angularapp1.client/src/proxy.conf.js
+++ angularapp1.client/src/proxy.conf.js
@@ -1,15 +1,14 @@
 const { env } = require('process');
-
-const target = env.ASPNETCORE_HTTPS_PORT ? `http://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:7105';
+const target = 'http://localhost:5019';
 
 const PROXY_CONFIG = [
   {
     context: [
       "/weatherforecast",
     ],
-    target,
-    secure: false
+    target: target,
+    secure: false,
+    changeOrigin: true // 增加這行可以避免一些開發環境的 Domain 問題
   }
 ]
 
